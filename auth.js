152396586721fc552cc323dff02f373a1ad4912d
krisdianwidya <krisdianwidya@gmail.com>
2021-01-04 11:12:02 +0700

    add login and logout functionality

--- auth.js
+++ auth.js
@@ -1,3 +1,4 @@
+// signup
 const btnSignup = document.querySelector('#btn-signup');
 
 btnSignup.addEventListener('click', () => {
@@ -11,3 +12,32 @@ btnSignup.addEventListener('click', () => {
             document.querySelector('.error').innerHTML = error.message;
         });
 });
+// end signup
+
+auth.onAuthStateChanged(user => {
+    console.log(user);
+});
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', e => {
+    e.preventDefault();
+    auth.signOut();
+});
+// end logout
+
+// login
+const btnLogin = document.querySelector('#btn-login');
+
+btnLogin.addEventListener('click', () => {
+    const email = document.querySelector('#email').value;
+    const password = document.querySelector('#password').value;
+    auth.signInWithEmailAndPassword(email, password)
+        .then(() => {
+            $('#modal-login').modal('hide');
+        })
+        .catch(error => {
+            document.querySelector('.error').innerHTML = error.message;
+        });
+});
+// end login
